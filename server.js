2671c3e2d47f797d5bf87561b582863900c21ec9
hino <hino@nile.cse.kyutech.ac.jp>
2019-10-23 16:21:13 +0900

    without callback , display height,hash,size

--- server.js
+++ server.js
@@ -4,35 +4,46 @@ const jayson   =  require('jayson/promise');
 const express  =  require('express')
 const app      =  express()
 const exec     =  require('child_process').exec;
+const execSync =  require('child_process').execSync;
 
 const client = jayson.client.tcp({
   port : 60401
 });
 
-app.get('/lenovo', async (req, res) => {
-  let COMMAND = 'bitcoin-cli getblockhash 277';
-  exec(COMMAND, function(error, stdout, stderr){
-    if(error !== null){
-      console.log('exec error: ' + error);
-      return;
-    }
-    console.log('block hash: ' + stdout);
-    COMMAND = 'bitcoin-cli getblock ' + stdout;
-    // getblock contents exe
-    exec(COMMAND, function(error, stdout, stderr){
-      if(error !== null){
-        console.log('exec error: ' + error);
-        return;
-      }
-      //console.log('block contents: ' + stdout);
-      let obj = JSON.parse(stdout);
-      console.log('size : ' + obj.size);
-      res.end('size : ' + obj.size);
 
-    }); 
-  });
-});
+//function blockSize(blockhight){
+//  let COMMAND = 'bitcoin-cli getblockhash ' + blockhight;
+//  exec(COMMAND, function(error, result){
+//    if(error !== null) { console.log('exec error: ' + error); return; }
+//    COMMAND = 'bitcoin-cli getblock ' + result;
+//    exec(COMMAND, function(error, result){
+//      if(error !== null) { console.log('exec error: ' + error); return; }
+//      let blockInfo = JSON.parse(result);
+//      //return blockInfo.size;
+//    });
+//    console.log(blockInfo);
+//  });
+//}
 
+
+let blockSize = function(height, callback, callback2){
+  let blockHash = callback(height);
+  let result = callback2(blockHash);
+  return result;
+};
+
+function getBlockHash(height){
+  let COMMAND = 'bitcoin-cli getblockhash ' + height;
+  let result  = execSync(COMMAND).toString();
+  return result;
+}
+
+function getBlock(blockHash){
+  let COMMAND = 'bitcoin-cli getblock ' + blockHash;
+  let result = execSync(COMMAND).toString();
+  result = JSON.parse(result);
+  return result.size;
+}
 
 app.get('/block_list', (req, res) => {
   client.request('blockchain.headers.subscribe',[],function(error, result){
@@ -40,15 +51,15 @@ app.get('/block_list', (req, res) => {
     let count = 25;
     let start_height = height - count;
     let displayList = "";
-    let sizeList = "";
-   
+
+    //blockSize(height, getBlockHash, getBlock); 
     client.request('blockchain.block.headers',[start_height,count,0],function(error, result){
       let output = result.result.hex.match(/.{160}/g);
       for( let i=0; i<count; i++){
-        displayList += (height-i) + " : " + output[i] + " : "  + "\n";
+        displayList += (height-i) + " : " + output[i] + " : " + blockSize((height-i), getBlockHash, getBlock)  + "\n";
       }
       console.log(displayList);
-      //res.end(displayList);
+      res.end(displayList);
     });
   });
 });
