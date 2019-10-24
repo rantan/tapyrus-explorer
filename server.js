4050d435acd565b75572a133dc5c9e92bd8ab604
hino <hino@nile.cse.kyutech.ac.jp>
2019-10-24 09:29:16 +0900

    get single block Info JSON style

--- server.js
+++ server.js
@@ -4,62 +4,44 @@ const jayson   =  require('jayson/promise');
 const express  =  require('express')
 const app      =  express()
 const exec     =  require('child_process').exec;
-const execSync =  require('child_process').execSync;
 
 const client = jayson.client.tcp({
   port : 60401
 });
 
 
-//function blockSize(blockhight){
-//  let COMMAND = 'bitcoin-cli getblockhash ' + blockhight;
-//  exec(COMMAND, function(error, result){
-//    if(error !== null) { console.log('exec error: ' + error); return; }
-//    COMMAND = 'bitcoin-cli getblock ' + result;
-//    exec(COMMAND, function(error, result){
-//      if(error !== null) { console.log('exec error: ' + error); return; }
-//      let blockInfo = JSON.parse(result);
-//      //return blockInfo.size;
-//    });
-//    console.log(blockInfo);
-//  });
-//}
-
-
-let blockSize = function(height, callback, callback2){
-  let blockHash = callback(height);
-  let result = callback2(blockHash);
-  return result;
-};
-
-function getBlockHash(height){
+function getBlockHash(height,callback){
   let COMMAND = 'bitcoin-cli getblockhash ' + height;
-  let result  = execSync(COMMAND).toString();
-  return result;
+  exec(COMMAND, function(error, result){
+    if(error !== null) { console.log('exec error: ' + error); return; }
+    callback(result,returnResult);
+  });
 }
 
-function getBlock(blockHash){
+function getBlock(blockHash,callback){
   let COMMAND = 'bitcoin-cli getblock ' + blockHash;
-  let result = execSync(COMMAND).toString();
-  result = JSON.parse(result);
-  return result.size;
+  exec(COMMAND, function(error, result){
+    if(error !== null) { console.log('exec error: ' + error); return; }
+    let blockInfo = JSON.parse(result);
+    callback(blockInfo);
+  });
 }
 
-app.get('/block_list', (req, res) => {
+
+app.get('/block', (req, res) => {
   client.request('blockchain.headers.subscribe',[],function(error, result){
     let height = result.result.height;
     let count = 25;
     let start_height = height - count;
     let displayList = "";
 
-    //blockSize(height, getBlockHash, getBlock); 
     client.request('blockchain.block.headers',[start_height,count,0],function(error, result){
       let output = result.result.hex.match(/.{160}/g);
-      for( let i=0; i<count; i++){
-        displayList += (height-i) + " : " + output[i] + " : " + blockSize((height-i), getBlockHash, getBlock)  + "\n";
-      }
-      console.log(displayList);
-      res.end(displayList);
+      getBlockHash(height, (hash) => {
+        getBlock(hash, (blockInfo) => {
+          res.json(blockInfo);
+        });
+      });
     });
   });
 });
