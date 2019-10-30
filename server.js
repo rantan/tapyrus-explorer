c9a2de0e3f0a0c9d9ea21bfa4d9cbe2658153d65
hino <hino@nile.cse.kyutech.ac.jp>
2019-10-30 13:10:33 +0900

    Supports URL input

--- server.js
+++ server.js
@@ -14,44 +14,35 @@ const cl      = new Client ({
   port     : 18443
 });
 
-const client  = jayson.client.tcp({
-  port : 60401
-});
-
-function getBlockHash(height, callback){
-  cl.getBlockHash(height).then((result) => callback(result));
-};
-
 function getBlock(blockHash, callback){
   cl.getBlock(blockHash).then((result) => callback(result));
 };
 
-app.get('/block', (req, res) => {
-  client.request('blockchain.headers.subscribe',[],function(error, result){
-    let height = result.result.height;
-    let count = 25;
-    let start_height = height - count;
+app.get('/blocks/:blockHash', (req, res) => {
     let output = new Object();
 
-      getBlockHash(height, (blockHash) => {
-        getBlock(blockHash, (blockInfo) => {
-          output.BlockHash             =  blockInfo.hash;
-          output.No_of_Transactions    =  blockInfo.nTx;
-          output.Height                =  blockInfo.height;
-          output.Timestamp             =  Date(blockInfo.time*1000);
-          output.Proof                 =  blockInfo.nonce;
-          output.SizeBytes             =  blockInfo.size;
-          output.Version               =  blockInfo.version;
-          output.Merkle_Root           =  blockInfo.merkleroot;
-          output.Immutable_Merkle_Root =  "immutable";
-          output.Previous_Block        =  blockInfo.previousblockhash;
-          output.Next_Block            =  blockInfo.nextblockhash;
-                   
-          res.json(output);
-          console.log(output);
+    let regex = new RegExp(/^[0-9a-fA-F]{64}$/);
+    let urlBlockHash = req.params.blockHash;
+
+    if(regex.test(urlBlockHash)){
+      getBlock(urlBlockHash, (blockInfo) => {
+        output.BlockHash             =  blockInfo.hash;
+        output.No_of_Transactions    =  blockInfo.nTx;
+        output.Height                =  blockInfo.height;
+        output.Timestamp             =  Date(blockInfo.time*1000);
+        output.Proof                 =  blockInfo.nonce;
+        output.SizeBytes             =  blockInfo.size;
+        output.Version               =  blockInfo.version;
+        output.Merkle_Root           =  blockInfo.merkleroot;
+        output.Immutable_Merkle_Root =  "immutable";
+        output.Previous_Block        =  blockInfo.previousblockhash;
+        output.Next_Block            =  blockInfo.nextblockhash;
+                 
+        res.json(output);
+        console.log(output);
       });
-    });
-  });
+    }
+    else{ res.status(400).send('Bad request'); }
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
