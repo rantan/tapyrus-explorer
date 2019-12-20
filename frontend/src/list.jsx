34d5bb19b9ddfac46cc709d53a0cbbeb6c50b523
hino <hino@nile.cse.kyutech.ac.jp>
2019-12-20 16:37:55 +0900

    list page nation, state have blockchaininfo

--- frontend/src/list.jsx
+++ frontend/src/list.jsx
@@ -2,38 +2,55 @@ import React from 'react';
 import axios from 'axios';
 import './index.css';
 import {
+  BrowserRouter as Router,
   Link,
   Route,
   Switch,
+  useLocation,
 } from 'react-router-dom';
+import queryString from 'querystring';
 import detail from './detail';
 
-
-const linesPerPage = 15;
+function GetParams() {
+  const location = window.location;
+  const queryParams = queryString.parse(location.search.slice(1));
+  return (
+    queryParams.page
+  );
+}
 
 class App extends React.Component {
   constructor(props, context) {
+    console.log("const");
     super(props, context);
     this.state = {
       data: [],
+      page: 1,
     };
   }
 
-  componentDidMount() {
+  componentDidMount() {console.log("didmount");
     this.getBlockInfo();
   }
 
-  static onClick() {}
-
   async getBlockInfo() {
-    const result = await axios.get(`${'http://localhost:3001/list'}/${linesPerPage}`);
+    const result = await axios.get(`${'http://localhost:3001/api/list'}`, {
+      params: {
+        page: GetParams(),
+        column: 15,
+      },
+    });
     this.setState({
       data: result.data,
+      page: GetParams(),
     });
   }
 
-  render() {
+  render() {console.log("render");
+  // const volvol = this.getBlockInfo();
+  // console.dir(volvol);
     const { data } = this.state;
+    const { page } = this.state;
     const list = data.map((i) => {
       const {
         hash, height, time, size,
@@ -45,13 +62,11 @@ class App extends React.Component {
           <td>{time}</td>
           <td>{size}</td>
           <Switch>
-            <Route path="block" component={detail} />
+            <Route path="/block" component={detail} />
           </Switch>
-
         </tr>
       );
     });
-
 
     list.sort(
       (a, b) => (a < b ? 1 : -1),
@@ -69,12 +84,26 @@ class App extends React.Component {
             {list}
           </tbody>
         </table>
+
         <div align="right">
-          <button type="button" onClick={this.onClick}>First</button>
-          <button type="button" onClick={this.onClick}>&lt;</button>
-          <button type="button" onClick={this.onClick}>Number</button>
-          <button type="button" onClick={this.onClick}>&gt;</button>
-          <button type="button" onClick={this.onClick}>Last</button>
+          {/* <button type="button" onClick={this.getBlockInfo.bind(this)}> */}
+          <button type="button">
+            <Link to={{
+              pathname: '/list',
+              search: '?page=1',
+            }}
+            >Prev
+            </Link>
+          </button>
+          <button type="button">
+          {/* <button type="button" onClick={this.getBlockInfo.bind(this)}> */}
+            <Link to={{
+              pathname: '/list',
+              search: '?page=2',//+{page}+1,,
+            }}
+            >Next 
+            </Link>
+          </button>
         </div>
       </div>
     );
