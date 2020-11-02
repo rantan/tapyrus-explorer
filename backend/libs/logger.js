d3ede966fa9d3c48b94b71486310deb6632c8a85
Takeshi Yasunaga <yasunaga@haw.co.jp>
2020-11-03 01:15:03 +0900

    supports log level environments variable.

--- backend/libs/logger.js
+++ backend/libs/logger.js
@@ -1,10 +1,12 @@
 const log4js = require('log4js');
+
+const LOG_LEVEL = process.env.TAPYRUS_EXPLORER_LOG_LEVEL || 'warn';
 log4js.configure({
   appenders: {
     everything: { type: 'stdout' }
   },
   categories: {
-    default: { appenders: ['everything'], level: 'warn' }
+    default: { appenders: ['everything'], level: LOG_LEVEL }
   }
 });
 
