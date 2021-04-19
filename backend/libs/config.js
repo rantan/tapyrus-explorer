fb7368e1c6009d26253384252c31861f74ec45b8
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2021-04-19 18:56:38 +0900

    Remove configuration for Electrum Protocol

--- backend/libs/config.js
+++ backend/libs/config.js
@@ -1,9 +1,5 @@
 const defaultConfig = {
   network: process.env.TAPYRUS_NETWORK || 'prod',
-  electrs: {
-    host: process.env.ELECTRS_RPC_HOST || 'localhost',
-    port: process.env.ELECTRS_RPC_PORT || 50001
-  },
   rest: {
     schema: process.env.ELECTRS_REST_SCHEMA || 'http',
     host: process.env.ELECTRS_REST_HOST || 'localhost',
